239bd6c8a1ec4cb23b6f23d71372f88222018572
EC2 Default User <ec2-user@ip-10-0-1-104.ec2.internal>
2025-01-29 08:35:26 +0000

    Updated auth

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,20 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { a, defineData, type ClientSchema } from '@aws-amplify/backend';
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
-export const auth = defineAuth({
-  loginWith: {
-    email: true,
-  },
+const schema = a.schema({
+  Todo: a.model({
+      content: a.string()
+    })
+    .authorization(allow => [allow.publicApiKey()])
+});
+
+// Used for code completion / highlighting when making requests from frontend
+export type Schema = ClientSchema<typeof schema>;
+
+// defines the data resource to be deployed
+export const data = defineData({
+  schema,
+  authorizationModes: {
+    defaultAuthorizationMode: 'apiKey',
+    apiKeyAuthorizationMode: { expiresInDays: 30 }
+  }
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,27 @@
 import { useEffect, useState } from "react";
 import type { Schema } from "../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
-
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+ 
 const client = generateClient<Schema>();
-
+ 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-
+ 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
       next: (data) => setTodos([...data.items]),
     });
   }, []);
-
+ 
   function createTodo() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
-
+ 
   return (
+    <Authenticator>
+      {({})=> (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
@@ -34,7 +38,9 @@ function App() {
         </a>
       </div>
     </main>
+     )}
+    </Authenticator>
   );
 }
-
+ 
 export default App;
